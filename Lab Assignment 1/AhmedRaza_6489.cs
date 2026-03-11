d9e4d65475793e5738d9e73204fc1bba58ba0344
Ahmed Raza <145737642+Ahmed-Progs@users.noreply.github.com>
2026-03-11 10:59:53 +0500

    Add files via upload

---
+++ Lab Assignment 1/AhmedRaza_6489.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhmedRaza_6489
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Q1--------------
+            //int first_number = 10;
+            //int second_number = 20;
+
+            //if (first_number > second_number)
+            //{
+            //    Console.WriteLine("The largest number is " + first_number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The Largest number is " + second_number);
+            //}
+
+            //Q2--------------
+            //int first_Number = 10;
+            //int second_Number = 50;
+            //int third_Number = 30;
+
+            //if (first_Number > second_Number && first_Number > third_Number)
+            //{
+            //    Console.WriteLine("The largest number is " + first_Number);
+            //}
+            //else if (second_Number > first_Number && second_Number > third_Number)
+            //{
+            //    Console.WriteLine("The largest number is " + second_Number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The largest number is " + third_Number);
+            //}
+
+            //Q3---------------
+            //int first_number = 10;
+            //int second_number = 20;
+
+            //if (first_number > second_number)
+            //{
+            //    Console.WriteLine("The Numbers in increasing orders is " + second_number + " , " + first_number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The Numbers in increasing orders is " + first_number + " , " + second_number);
+            //}
+
+
+
+            //Q4----------------
+            //int first_number = 10;
+            //int second_number = 20;
+            //int third_number = 30;
+
+            //if (first_number > second_number && first_number > third_number)
+            //{
+            //    if (second_number > third_number)
+            //    {
+            //        Console.WriteLine("The Numbers in increasing orders are " + third_number + " , " + second_number + " , " + first_number);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The Numbers in increasing orders are " + second_number + " , " + third_number + " , " + first_number);
+            //    }
+            //}
+            //else if (second_number > third_number)
+            //{
+            //    if (first_number > third_number)
+            //    {
+            //        Console.WriteLine("The Numbers in increasing orders are " + third_number + " , " + first_number + " , " + second_number);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The Numbers in increasing orders are " + first_number + " , " + third_number + " , " + second_number);
+            //    }
+            //}
+            //else
+            //{
+            //    if (first_number > second_number)
+            //    {
+            //        Console.WriteLine("The Numbers in increasing orders are " + second_number + " , " + first_number + " , " + third_number);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The Numbers in increasing orders are " + first_number + " , " + second_number + " , " + third_number);
+            //    }
+
+            //}
+
+
+            //Q5----------------------
+            //int first_number = 10;
+            //int second_number = 20;
+
+            //if (first_number < second_number)
+            //{
+            //    Console.WriteLine("The smallest number is " + first_number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The smallest number is " + second_number);
+            //}
+
+            //Q6----------------------
+            //int first_number = 10;
+            //int second_number = 20;
+            //int third_number = 30;
+
+            //if (first_number < second_number && first_number < third_number)
+            //{
+            //    Console.WriteLine("The smallest number is " + first_number);
+            //}
+            //else if (second_number < first_number && second_number < third_number)
+            //{
+            //    Console.WriteLine("The smallest number is " + second_number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The smallest number is " + third_number);
+            //}
+
+            //Q7----------------------
+            //int first_number = 10;
+            //int second_number = 20;
+
+            //if (first_number < second_number)
+            //{
+            //    Console.WriteLine("The numbers in decreasing order are " + second_number + " , " + first_number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The numbers in decreasing order are " + first_number + " , " + second_number);
+            //}
+
+
+            //Q8-------------------
+            //int first_number = 10;
+            //int second_number = 20;
+            //int third_number = 30;
+
+            //if (first_number < second_number && first_number < third_number)
+            //{
+            //    if (second_number < third_number)
+            //    {
+            //        Console.WriteLine("The numbers in decreasing order are " + third_number + " , " + second_number + " , " + first_number);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The numbers in decreasing order are " + second_number + " , " + third_number + " , " + first_number);
+            //    }
+            //}
+            //else if (second_number < first_number && second_number < third_number)
+            //{
+            //    if (first_number < third_number)
+            //    {
+            //        Console.WriteLine("The numbers in decreasing order are " + third_number + " , " + first_number + " , " + second_number);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The numbers in decreasing order are " + first_number + " , " + third_number + " , " + second_number);
+            //    }
+            //}
+            //else
+            //{
+            //    if (first_number < second_number)
+            //    {
+            //        Console.WriteLine("The numbers in decreasing order are " + second_number + " , " + first_number + " , " + third_number);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The numbers in decreasing order are " + first_number + " , " + second_number + " , " + third_number);
+            //    }
+            //}
+
+            //Q9---------------------
+            //int first_number = 10;
+            //int second_number = 20;
+            //int third_number = 30;
+            //double average_number = (first_number + second_number + third_number) / 3;
+
+            //Console.WriteLine("The average of three numbers " + first_number + " , " + second_number + " , " + third_number + " is " + average_number );
+
+            //Q10------------------
+            //int student_score = 80;
+
+            //if (student_score >= 90)
+            //{
+            //    Console.WriteLine("Your Grade in this subject is A+.");
+            //}
+            //else if (student_score >= 80 && student_score <= 89)
+            //{
+            //    Console.WriteLine("Your Grade in this subject is A.");
+            //}
+            //else if (student_score >= 70 && student_score <= 79)
+            //{
+            //    Console.WriteLine("Your Grade in this subject is B.");
+            //}
+            //else if (student_score >= 60 && student_score <= 69)
+            //{
+            //    Console.WriteLine("Your Grade in this subject is C.");
+            //}
+            //else if (student_score >= 50 && student_score <= 59)
+            //{
+            //    Console.WriteLine("Your Grade in this subject is D.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your Grade in this subject is F.");
+            //}
+
+            //Q11-----------------
+            //int user_age = 18;
+
+            //if (user_age < 18)
+            //{
+            //    Console.WriteLine("You are a child.");
+            //}
+            //else if (user_age < 65)
+            //{
+            //    Console.WriteLine("You are an adult.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are a senior citizen.");
+            //}
+
+
+            //Q15----------------
+            //int a = -2;
+            //int b = 3;
+            //int c = -4;
+            //int d = b * b - 4 * a * c;
+
+            //if (d > 0)
+            //{
+            //    double x1 = (-b + Math.Sqrt(d)) / (2.0 * a);
+            //    double x2 = (-b - Math.Sqrt(d)) / (2.0 * a);
+            //    Console.WriteLine("By using the formula ax^2 + bx + c = 0, the value of x is " + x1);
+            //    Console.WriteLine("By using the formula ax^2 + bx + c = 0, the value of x is " + x2);
+            //}
+            //else if (d == 0)
+            //{
+            //    double x = -b / (2 * a);
+            //    Console.WriteLine("By using the formula ax^2 + bx + c = 0, the value of x is " + x);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The equation has no real roots.");
+            //}
+
+        }
+
+    }
+}
+
